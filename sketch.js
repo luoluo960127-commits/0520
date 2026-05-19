9b12a22e140248094313184871eee6f074433687
luoluo960127-commits <luoluo960127@gmail.com>
2026-05-19 10:18:55 +0800

    1.1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let video;
+
 function setup() {
-  createCanvas(400, 400);
+  // 產生一個全螢幕的畫布
+  createCanvas(windowWidth, windowHeight);
+  // 擷取攝影機影像
+  video = createCapture(VIDEO);
+  // 隱藏預設的 HTML 影片元件，避免重複顯示
+  video.hide();
 }
 
 function draw() {
-  background(220);
+  // 畫布背景顏色為 e7c6ff
+  background('#e7c6ff');
+
+  // 計算影像顯示的寬高（畫布寬高的 60%）並置中
+  let vW = width * 0.6;
+  let vH = height * 0.6;
+  let x = (width - vW) / 2;
+  let y = (height - vH) / 2;
+
+  // 將影像繪製在畫布中間
+  image(video, x, y, vW, vH);
+}
+
+function windowResized() {
+  // 當視窗大小改變時，重新調整畫布大小
+  resizeCanvas(windowWidth, windowHeight);
 }
